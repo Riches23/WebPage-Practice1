eb45db2e69e2db8708233df479818ee4b7b10f45
Riches23 <richesetim604@gmail.com>
2020-09-15 21:34:57 +0100

    python basics1

---
+++ Improvement2/pythonbasics.py
@@ -0,0 +1,65 @@
+"Hello"
+print('hello')
+print("python is awesome!")
+print('After all, "tomorrow" is just another day')
+print(27)
+print(2+7)
+print('I am learning python')# I am new here
+a="Hello"
+print(a)
+b=10
+print(b)
+int(9)
+Car="BMW"
+car="Jaguwa"
+CAR="Honda"
+print(Car)
+print(car)
+print(CAR)
+My_Name="Riches Etim"
+print(My_Name)
+print("Hi There, Welcome to Python")
+print("Hi There, \nWelcome to Python!")
+print(12345)
+print("\t1\t2\t3\t4\t5")
+print("Hello\tWorld")
+print("Hello World")
+a="Give Love"
+print(a)
+print(len(a))
+b="Mark"
+name="Shelock here"
+print(name)
+print(len(name))
+print(len(name[9:]))
+print(name[9:])
+print(name[-4])
+print(name[0:3]) # Upper bound Excluded
+print(My_Name[0:6])
+print(My_Name[3:6])
+print(name[::]) #Step Size
+print(name[::3])
+# [Start: Stop: Step Size]
+print(name[0:7:2])
+print(name[0:7:3])
+print(name[0:7:4])
+print(name[::-1])
+print(name[::-2])
+boy="Sam"
+print(boy)
+print(boy[1:])
+b=(boy[1:])
+print("P"+ b)
+x="Hello Boo,"
+y="Let's go for a walk"
+print(x +" " +y)
+print(x+"\t"+y)
+p="Love it"
+print(p.lower())
+print(p.upper())
+print(p.split())
+print(p.split("u"))
+print(p.split("v"))
+print("My name is {}".format("Etim Riches"))
+print("The colors are {} {} {}".format("Red","Yellow","Blue"))
+print("The colors are {} {} {}".format("Red,","Yellow, and","Blue"))
